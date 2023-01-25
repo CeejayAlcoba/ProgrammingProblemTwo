99a89e11fa082b0e376bd2447f7b2ecc495d4615
cj alcoba <smithkim909@gmail.com>
2023-01-25 21:14:00 +0800

    if password is null

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -55,24 +55,31 @@ namespace Services
         public Employee UpdateEmployee(Employee employee)
         {
             var getEmployeeById = _unitOfWork.Employees.GetEmployeeById(employee.employeeId);
-            var toSalt = _accountInterface.GenerateSalt(
-                employee.credential.password
-                );
-            var toHash = _accountInterface.GenerateHashPassword(
-                employee.credential.password,
-                toSalt
-                );
-            getEmployeeById.firstname = employee.firstname;
-            getEmployeeById.lastname = employee.lastname;
-            getEmployeeById.gender = employee.gender;
-            getEmployeeById.birthday = employee.birthday;
-            getEmployeeById.positionId = employee.positionId;
-            getEmployeeById.credential.username = employee.credential.username;
+            if (employee.credential.password == null)
+            {
+                getEmployeeById.firstname = employee.firstname;
+                getEmployeeById.lastname = employee.lastname;
+                getEmployeeById.gender = employee.gender;
+                getEmployeeById.birthday = employee.birthday;
+                getEmployeeById.positionId = employee.positionId;
+                getEmployeeById.credential.username = employee.credential.username;
+            }
+            else
+            {
+                var toSalt = _accountInterface.GenerateSalt(
+                    employee.credential.password
+                    );
+                var toHash = _accountInterface.GenerateHashPassword(
+                    employee.credential.password,
+                    toSalt
+                    );
 
-            getEmployeeById.credential.salted = toSalt;
-            getEmployeeById.credential.hashed = toHash;
+                getEmployeeById.credential.salted = toSalt;
+                getEmployeeById.credential.hashed = toHash;
+            }
             _unitOfWork.Complete();
             return getEmployeeById;
+
         }
         public void DeleteEmployee(int id)
         {
